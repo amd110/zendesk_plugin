9d3420c8224bc86ca523b99a68c5aa53f30b71da
sudan <ati110@163.com>
2025-05-29 15:16:14 +0800

    优化 android 相关代码 并在 example 的  manifest 增加 MessagingActivity 的 theme 防止崩溃

--- android/src/main/kotlin/com/teppei/kikuchi/zendesk/plugin/ZendeskPlugin.kt
+++ android/src/main/kotlin/com/teppei/kikuchi/zendesk/plugin/ZendeskPlugin.kt
@@ -1,8 +1,6 @@
 package com.teppei.kikuchi.zendesk.plugin
 
 import android.app.Activity
-import androidx.annotation.NonNull
-import com.zendesk.logger.Logger
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -10,7 +8,11 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import zendesk.chat.*
+import zendesk.chat.Chat
+import zendesk.chat.ChatConfiguration
+import zendesk.chat.ChatEngine
+import zendesk.chat.ChatMenuAction
+import zendesk.chat.VisitorInfo
 import zendesk.classic.messaging.MessagingActivity
 
 
@@ -21,21 +23,24 @@ class ZendeskPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     // / This local reference serves to register the plugin with the Flutter Engine and unregister it
     // / when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
-    private lateinit var activity: Activity
+    private var activity: Activity? = null
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "zendesk")
         channel.setMethodCallHandler(this)
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 
     override fun onDetachedFromActivity() {
+        activity = null
     }
 
+
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
@@ -43,47 +48,54 @@ class ZendeskPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "getPlatformVersion" -> {
                 result.success("Android ${android.os.Build.VERSION.RELEASE}")
             }
+
             "initialize" -> {
                 initialize(call)
                 result.success(true)
             }
+
             "setVisitorInfo" -> {
                 setVisitorInfo(call)
                 result.success(true)
             }
+
             "startChat" -> {
                 startChat(call)
                 result.success(true)
             }
+
             "addTags" -> {
                 addTags(call)
                 result.success(true)
             }
+
             "removeTags" -> {
                 removeTags(call)
                 result.success(true)
             }
+
             else -> {
                 result.notImplemented()
             }
         }
     }
 
-    fun initialize(call: MethodCall) {
+    private fun initialize(call: MethodCall) {
         val accountKey = call.argument<String>("accountKey") ?: ""
         val applicationId = call.argument<String>("appId") ?: ""
 
-        Chat.INSTANCE.init(activity, accountKey, applicationId)
+        activity?.let { Chat.INSTANCE.init(it, accountKey, applicationId) }
     }
 
-    fun setVisitorInfo(call: MethodCall) {
+    private fun setVisitorInfo(call: MethodCall) {
         val name = call.argument<String>("name") ?: ""
         val email = call.argument<String>("email") ?: ""
         val phoneNumber = call.argument<String>("phoneNumber") ?: ""
@@ -93,45 +105,48 @@ class ZendeskPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val chatProvider = Chat.INSTANCE.providers()?.chatProvider()
 
         val visitorInfo = VisitorInfo.builder()
-                .withName(name)
-                .withEmail(email)
-                .withPhoneNumber(phoneNumber) // numeric string
-                .build()
+            .withName(name)
+            .withEmail(email)
+            .withPhoneNumber(phoneNumber) // numeric string
+            .build()
         profileProvider?.setVisitorInfo(visitorInfo, null)
         chatProvider?.setDepartment(department, null)
     }
 
-    fun addTags(call: MethodCall) {
-        val tags = call.argument<List<String>>("tags") ?: listOf<String>()
+    private fun addTags(call: MethodCall) {
+        val tags = call.argument<List<String>>("tags") ?: listOf()
         val profileProvider = Chat.INSTANCE.providers()?.profileProvider()
         profileProvider?.addVisitorTags(tags, null)
     }
 
-    fun removeTags(call: MethodCall) {
-        val tags = call.argument<List<String>>("tags") ?: listOf<String>()
+    private fun removeTags(call: MethodCall) {
+        val tags = call.argument<List<String>>("tags") ?: listOf()
         val profileProvider = Chat.INSTANCE.providers()?.profileProvider()
         profileProvider?.removeVisitorTags(tags, null)
     }
 
-    fun startChat(call: MethodCall) {
+    private fun startChat(call: MethodCall) {
         val isPreChatFormEnabled = call.argument<Boolean>("isPreChatFormEnabled") ?: true
-        val isAgentAvailabilityEnabled = call.argument<Boolean>("isAgentAvailabilityEnabled") ?: true
-        val isChatTranscriptPromptEnabled = call.argument<Boolean>("isChatTranscriptPromptEnabled") ?: true
+        val isAgentAvailabilityEnabled =
+            call.argument<Boolean>("isAgentAvailabilityEnabled") ?: true
+        val isChatTranscriptPromptEnabled =
+            call.argument<Boolean>("isChatTranscriptPromptEnabled") ?: true
         val isOfflineFormEnabled = call.argument<Boolean>("isOfflineFormEnabled") ?: true
         val title = call.argument<String>("title") ?: ""
         val chatConfigurationBuilder = ChatConfiguration.builder()
         chatConfigurationBuilder
-                .withAgentAvailabilityEnabled(isAgentAvailabilityEnabled)
-                .withTranscriptEnabled(isChatTranscriptPromptEnabled)
-                .withOfflineFormEnabled(isOfflineFormEnabled)
-                .withPreChatFormEnabled(isPreChatFormEnabled)
-                .withChatMenuActions(ChatMenuAction.END_CHAT)
+            .withAgentAvailabilityEnabled(isAgentAvailabilityEnabled)
+            .withTranscriptEnabled(isChatTranscriptPromptEnabled)
+            .withOfflineFormEnabled(isOfflineFormEnabled)
+            .withPreChatFormEnabled(isPreChatFormEnabled)
+            .withChatMenuActions(ChatMenuAction.END_CHAT)
 
         val chatConfiguration = chatConfigurationBuilder.build()
-
-        MessagingActivity.builder()
+        activity?.let {
+            MessagingActivity.builder()
                 .withToolbarTitle(title)
                 .withEngines(ChatEngine.engine())
-                .show(activity, chatConfiguration)
+                .show(it, chatConfiguration)
+        }
     }
 }
